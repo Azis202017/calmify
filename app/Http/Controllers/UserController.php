fa458d1e6110c97e9ab81c229c6fe88f7cad322d
Azis202017 <azisa6980@gmail.com>
2023-05-17 07:15:05 +0700

    [UserController] added functionality about authentication

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+
+        $user = Auth::user();
+        return response()->json($user, 200);
+    }
+    public function login(Request $request)
+    {
+        try {
+            $request->validate([
+                'email' => 'email|required',
+                'password' => 'required'
+            ]);
+
+            $credentials = request(['email', 'password']);
+            if (!Auth::attempt($credentials)) {
+                return response()->error([
+                    'message' => 'Unauthorized'
+                ], 500);
+            }
+
+            $user = User::where('email', $request->email)->first();
+            if (!Hash::check($request->password, $user->password, [])) {
+                throw new \Exception('Invalid Credentials');
+            }
+
+            $tokenResult = $user->createToken('calmifyToken')->plainTextToken;
+            return response()->json([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ], 200);
+        } catch (Exception $error) {
+            return response()->json([
+                'message' => 'Something went wrong',
+                'error' => $error->getMessage(),
+            ], 500);
+        }
+    }
+    public function register(Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255',],
+            ]);
+
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            $user = User::where('email', $request->email)->first();
+
+
+            return response()->json([
+                'token_type' => 'Bearer',
+                'message' => 'Berhasil registrasi',
+                'user' => $user
+            ], 200);
+        } catch (Exception $error) {
+            return response()->json([
+                'message' => 'Sesuatu ada yang salah',
+                'error' => $error,
+            ], 200);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Token berhasil dihapus']);
+    }
+}
