e8b901b9778995d542b9af0abee6dd6c0e08fe05
Azis202017 <azisa6980@gmail.com>
2023-05-17 21:50:14 +0700

    [User] added sertifikasi

--- database/migrations/2023_05_17_003403_add_column_to_users_table.php
+++ database/migrations/2023_05_17_003403_add_column_to_users_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->enum('role', ['pengguna', 'psikolog']);
+            $table->string('ijazah');
+            $table->string('sertifikasi');
         });
     }
 
