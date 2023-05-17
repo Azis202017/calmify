6ecd0730874927581c4c281d838f029433ded6a5
Azis202017 <azisa6980@gmail.com>
2023-05-17 07:45:10 +0700

    [API] added routes for psikolog

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\MetodePembayaranController;
+use App\Http\Controllers\PsikologController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +16,16 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::middleware(['auth:sanctum','dokter'])->group(function() {
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
 });
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('/user', [UserController::class, 'index']);
+    Route::post('/logout',[UserController::class,'logout']);
+});
+
+Route::post('register',[UserController::class,'register']);
+Route::post('login',[UserController::class,'login']);
 Route::post('/metode/add', [MetodePembayaranController::class,'store']);
 Route::get('/metode/show', [MetodePembayaranController::class,'index']);
+Route::get('/psikolog',[PsikologController::class,'index']);
