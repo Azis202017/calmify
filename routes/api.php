142ad741e2c20857e6d96d5e396d6273b7dbdbf9
Azis202017 <azisa6980@gmail.com>
2023-05-18 08:29:55 +0700

    [API] added deleted to delete a article

--- routes/api.php
+++ routes/api.php
@@ -8,19 +8,11 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 Route::middleware(['auth:sanctum','psikolog'])->group(function() {
     Route::post('/article/add', [ArticleController::class,'store']);
     Route::put('/article/ubah/{id}', [ArticleController::class,'update']);
+    Route::delete('/article/{id}', [ArticleController::class,'delete']);
+
     Route::get('/article/user/{userID}',[ArticleController::class,'getArticleUser']);
 });
 Route::middleware('auth:sanctum')->group(function() {
