6ec2f05d28b564315504de71535d69b48d8cc6d0
Azis202017 <azisa6980@gmail.com>
2023-05-27 18:40:06 +0700

    [Waktu Sesi] rollback

--- app/Http/Controllers/SesiWaktuController.php
+++ app/Http/Controllers/SesiWaktuController.php
@@ -3,20 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Models\SesiWaktu;
-use App\Models\User;
 use Illuminate\Http\Request;
 
 class SesiWaktuController extends Controller
 {
-    public function index($id) {
-        $sesiWaktu = User::with(['sesiWaktu'])->where('id', '=', $id)->get();
+    public function index($id)
+    {
+        $sesiWaktu = SesiWaktu::with(['users'])->where('id_user', '=', $id)->get();
         return response()->json(
             [
                 'data' => $sesiWaktu
-            ], 200
+            ],
+            200
         );
     }
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         return response()->json([
             'data' => SesiWaktu::create(['waktu_sesi' => $request->waktu_sesi, 'id_user' => $request->id_user])
         ]);
