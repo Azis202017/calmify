d692ecd91a3709845a2d56913d4adcce0c17489c
Azis202017 <azisa6980@gmail.com>
2023-05-27 18:25:52 +0700

    [Sesi Waktu] get all data sesi waktu

--- app/Http/Controllers/SesiWaktuController.php
+++ app/Http/Controllers/SesiWaktuController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\SesiWaktu;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class SesiWaktuController extends Controller
 {
     public function index($id) {
-        $sesiWaktu = SesiWaktu::with(['users'])->where('id_user', '=', $id)->get();
+        $sesiWaktu = User::with(['sesiWaktu'])->where('id', '=', $id)->get();
         return response()->json(
             [
                 'data' => $sesiWaktu
