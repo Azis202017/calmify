1d979252091d7d1d6078d84154edd5025c5bc1f3
Azis202017 <azisa6980@gmail.com>
2023-05-28 23:23:53 +0700

    [Pemesanan] added pemesanan

--- app/Models/pemesanan.php
+++ app/Models/pemesanan.php
@@ -2,11 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Session\Session;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class pemesanan extends Model
 {
     use HasFactory;
-    
+    protected $fillable = [
+        'id_user',
+        'id_metode_pembayaran',
+        'no_telepon',
+        'tanggal_lahir',
+        'alamat',
+        'tanggal_sesi',
+        'id_waktu_sesi',
+        'durasi'
+    ];
+    public function users() {
+        return $this->belongsTo(User::class, 'id_user','id');
+
+    }
+    public function metode_pembayaran() {
+        return $this->belongsTo(MetodePembayaran::class, 'id_metode_pembayaran','id');
+
+    }
+    public function waktu_sesi(){
+        return $this->belongsTo(Session::class, 'id_waktu_sesi','id');
+    }
 }
