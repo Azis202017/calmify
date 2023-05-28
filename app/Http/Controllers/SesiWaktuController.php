e9d20f787918246cc0096991fa36f32e64692e41
Azis202017 <azisa6980@gmail.com>
2023-05-28 18:12:47 +0700

    [Waktu Sesi] added waktu sesi

--- app/Http/Controllers/SesiWaktuController.php
+++ app/Http/Controllers/SesiWaktuController.php
@@ -18,9 +18,15 @@ class SesiWaktuController extends Controller
         );
     }
     public function store(Request $request)
+
     {
-        return response()->json([
-            'data' => SesiWaktu::create(['waktu_sesi' => $request->waktu_sesi, 'id_user' => $request->id_user])
-        ]);
+        if (SesiWaktu::where('waktu_sesi', '=', $request->waktu_sesi)->first() && SesiWaktu::where('id_user', '=', $request->id_user)->first()) {
+            return response()->json(['data' =>  SesiWaktu::create(['waktu_sesi' => $request->waktu_sesi, 'id_user' => $request->id_user])], 200);
+        }else {
+            return response()->json(['message' => 'Waktu sesi sudah ada']);
+        }
+        // return response()->json([
+        //     'data' =>)
+        // ]);
     }
 }
