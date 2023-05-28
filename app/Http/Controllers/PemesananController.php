3656a926e5aa7963488c7ca1b40b7b73e0870fa0
Azis202017 <azisa6980@gmail.com>
2023-05-28 23:24:06 +0700

    Pemesanan

--- app/Http/Controllers/PemesananController.php
+++ app/Http/Controllers/PemesananController.php
@@ -2,9 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\pemesanan;
+use Exception;
 use Illuminate\Http\Request;
 
 class PemesananController extends Controller
 {
-    //
+    public function index()
+    {
+        $pemesanan = pemesanan::with(['user', 'waktu_sesi', 'metode_pembayaran'])->get();
+        return response()->json([
+            'message' => 'invoice',
+            'data' => $pemesanan,
+
+
+        ], 200);
+    }
+    public function store(Request $request)
+    {
+        try {
+            $data =    pemesanan::create(['id_user' => $request->id_user, 'id_metode_pembayaran' => $request->id_metode_pembayaran, 'no_telepon' => $request->no_telepon, 'tanggal_lahir' => $request->tanggal_lahir,'alamat' => $request->alamat, 'tanggal_sesi' => $request->tanggal_sesi, 'id_waktu_sesi' => $request->id_waktu_sesi, 'durasi' => $request->durasi, 'no_telepon' => $request->no_telepon]);
+         ;
+            return response()->json([
+               'message' => 'success memesan psikolog',
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
 }
